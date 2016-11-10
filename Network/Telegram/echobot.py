ca2dea659f43c701df3c96b9dbc6550f71c57a62
Arash Hatami <hatamiarash7@gmail.com>
2016-11-10 23:18:14 +0330

    change telegram code

--- Network/Telegram/echobot.py
+++ Network/Telegram/echobot.py
@@ -1,46 +1,37 @@
 # -*- coding: utf-8 -*-
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
-def start(bot, update):
-    update.message.reply_text('Hi!')
+def command_start(bot, update):
+    update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
 
-def helpp(bot, update):
-    update.message.reply_text('Help!')
+def command_help(bot, update):
+    update.message.reply_text('what can i do for you ??')
 
-def echo(bot, update):
-    update.message.reply_text(update.message.text)
+def command_echo(bot, update):
+    print(update.message.text)
+    #update.message.reply_text(update.message.text)
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 def main():
-    # Create the EventHandler and pass it your bot's token.
-    updater = Updater("260306319:AAFyc6Et3OaTu-QHQQwN2wo0KvjbT7X9Wkg")
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", helpp))
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler([Filters.text], echo))
+    # Create the EventHandler
+    up = Updater("260306319:AAFyc6Et3OaTu-QHQQwN2wo0KvjbT7X9Wkg")
+    dp = up.dispatcher
+    # commands
+    dp.add_handler(CommandHandler("start", command_start))
+    dp.add_handler(CommandHandler("help", command_help))
+    dp.add_handler(MessageHandler([Filters.text], command_echo))
     # log all errors
     dp.add_error_handler(error)
     # Start the Bot
-    updater.start_polling()
-    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    up.start_polling()
+    up.idle()
 
 if __name__ == '__main__':
     main()
